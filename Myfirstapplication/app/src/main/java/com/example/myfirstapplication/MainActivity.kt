8d7cc3c4331ba8d6d376c9b101845e2e70ba0cd7
Armando Martí Carretero <jarman75@hotmail.es>
2019-08-27 00:10:44 +0200

    my first app

---
+++ Myfirstapplication/app/src/main/java/com/example/myfirstapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myfirstapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.view.View
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    /** Called when the user taps the Send button */
+    fun sendMessage(view: View) {
+        val editText = findViewById<EditText>(R.id.edName)
+        val message = editText.text.toString()
+        val intent = Intent(this, DisplayMessageActivity::class.java).apply {
+            putExtra(EXTRA_MESSAGE, message)
+        }
+        startActivity(intent)
+
+    }
+
+}
