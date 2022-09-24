349f6b00d1e947024e7d548d432f0216dab9d927
Armando Martí Carretero <jarman75@hotmail.es>
2022-09-24 13:11:17 +0200

    rollldice challenger

--- latest/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ latest/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -10,15 +10,34 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val rollButton : Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
+
+        val countButton : Button = findViewById(R.id.count_up)
+        countButton.setOnClickListener { countUp() }
+    }
+
+    private fun countUp() {
+        Toast.makeText(this, "countUp button clicked",
+            Toast.LENGTH_SHORT).show()
+
+        val resultText : TextView = findViewById(R.id.result_text)
+        if (resultText.text.toString() == "Hello World!")
+            resultText.text = "1"
+        else if (resultText.text.toString() != "6")
+            resultText.text = (resultText.text.toString().toShort() + 1).toString()
+
+
     }
 
     private fun rollDice(){
-        Toast.makeText(this, "button clicked",
+        Toast.makeText(this, "rollDice button clicked",
             Toast.LENGTH_SHORT).show()
+
         val randomInt = (1..6).random()
+
         val resultText : TextView = findViewById(R.id.result_text)
-        resultText.text = "Roll Dice $randomInt !!!"
+        resultText.text = randomInt.toString()
     }
 }
