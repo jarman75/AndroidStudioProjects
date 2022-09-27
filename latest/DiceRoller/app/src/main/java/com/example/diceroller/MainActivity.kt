10b6f34675bdcc31b2f710c6c9ba8862dda8be64
Armando Martí Carretero <jarman75@hotmail.es>
2022-09-27 13:37:07 +0200

    add clear button

--- latest/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ latest/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -16,19 +16,26 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice() }
 
+        val clearButton: Button = findViewById(R.id.clear_button)
+        clearButton.setOnClickListener { clearDice() }
+
         diceImage = findViewById(R.id.dice_image)
         diceImage2 = findViewById(R.id.dice_image2)
 
     }
 
+    private fun clearDice(){
+        diceImage.setImageResource(R.drawable.empty_dice)
+        diceImage2.setImageResource(R.drawable.empty_dice)
+    }
 
     private fun rollDice(){
 
         val randomInt = (1..6).random()
-
         val randomInt2 = (1..6).random()
+        val total = randomInt+randomInt2
 
-        Toast.makeText(this.diceImage.context, "It's $randomInt, $randomInt2 !!",
+        Toast.makeText(this.diceImage.context, "It's $total !!",
             Toast.LENGTH_SHORT).show()
 
         diceImage.setImageResource(getImage(randomInt))
